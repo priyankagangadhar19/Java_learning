c9277f07e5a4e417d9d0e9be33a5de5b2181e999
priyankagangadhar19 <priyankagangadhar19@gmail.com>
2020-10-08 17:43:02 +0530

    Commit

---
+++ src/Welcome_java.java
@@ -0,0 +1,13 @@
+
+public class Welcome_java {
+	public static void main(String[] args) {
+		System.out.println("WELCOME");
+		
+		Welcome_java.Sum(10, 3);
+	}
+	public static void Sum(int a, int b) {
+		int n=a+b;
+		System.out.println(n);
+		
+	}
+}
